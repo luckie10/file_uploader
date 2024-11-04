a3168e5adc93883cf55816fcc9125ab909ff9467
Boban Kilvin <btrajcevski10@gmail.com>
2024-11-04 05:03:12 -0500

    Add ejs templating

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import "module-alias/register";
+import express from "express";
+import path from "path";
 
 import config from "./config";
-import express from "express";
 import routes from "./routes";
 import sessionConfig from "./config/session";
 
 const app = express();
+
+app.set("views", path.join(import.meta.dirname, "view"));
+app.set("view engine", "ejs");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
